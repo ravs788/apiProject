3190d8cb70bc1c54b1fb04bc6df9fb0fc78c9269
ravs788 <ravs788@gmail.com>
2025-02-16 17:31:42 +0530

    Add a failing and a flaky test (#9)

--- src/test/java/TestSandbox.java
+++ src/test/java/TestSandbox.java
@@ -1,4 +1,7 @@
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+
+import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -10,9 +13,23 @@ public class TestSandbox {
         Thread.sleep(1000);
     }
 
+    @Tag("failing")
     @Test
     void assertThatDayIsDay() throws InterruptedException{
-        assertEquals("day","day","true is true");
+        assertEquals("day","night","true is true");
         Thread.sleep(1000);
+    }
+
+    @Tag("flaky")
+    @Test
+    void createAFlakyTest(){
+        Random random = new Random();
+        int value = random.nextInt(10000);
+        if(value%2==0){
+            assertTrue(true,"value is even");
+        }
+        else{
+            assertTrue(false, "value is odd");
+        }
     }
 }
