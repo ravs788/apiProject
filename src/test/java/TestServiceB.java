7f8048fcc5c9b636bea8a84dc0060aa024f438e9
ravs788 <ravs788@gmail.com>
2025-02-24 06:56:53 +0530

    Trigger tests on external event (#25)
    * added a new workflow file and additional test files
    
    * cleaned up unsused imports

---
+++ src/test/java/TestServiceB.java
@@ -0,0 +1,25 @@
+import com.typesafe.config.Config;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Tag;
+import org.ravs788.config.TestEnvFactory;
+import org.ravs788.config.annotations.SmokeTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@Tag("service-B")
+@Slf4j
+public class TestServiceB {
+
+    @SmokeTest
+    void assertThatTrueIsTrue(){
+        assertTrue(true, "true is true");
+    }
+
+    @SmokeTest
+    void assertThatTestForChosenEnvRuns(){
+        Config CONFIG = TestEnvFactory.getInstance().getConfig();
+        String expectedEnv = CONFIG.getString("TEST_ENV");
+        log.info("expectedEnv is {}", expectedEnv);
+        assertEquals(expectedEnv, CONFIG.getString("TEST_ENV"), "TEST_ENV");
+    }
+}
