7684dd8e27f4554afbee7f1a3bdbe88f02fce7a0
ravs788 <ravs788@gmail.com>
2025-03-10 06:54:37 +0530

    Add reporting to cloud elastic (#27)
    * added a new workflow file and additional test files
    
    * cleaned up unsused imports
    
    * added external reporting
    
    * added elastic logging reporting based on external event
    
    * added a cleaner interface to push to elastic cloud

--- src/main/java/org/ravs788/extensions/report/PublishResults.java
+++ src/main/java/org/ravs788/extensions/report/PublishResults.java
@@ -6,6 +6,7 @@ import co.elastic.clients.elasticsearch.core.IndexResponse;
 import co.elastic.clients.json.jackson.JacksonJsonpMapper;
 import co.elastic.clients.transport.ElasticsearchTransport;
 import co.elastic.clients.transport.rest_client.RestClientTransport;
+import com.typesafe.config.Config;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import lombok.extern.slf4j.Slf4j;
@@ -13,17 +14,22 @@ import org.apache.http.Header;
 import org.apache.http.HttpHost;
 import org.apache.http.message.BasicHeader;
 import org.elasticsearch.client.RestClient;
+import org.ravs788.config.TestEnvFactory;
 
 import java.io.IOException;
 
+import static org.ravs788.extensions.report.ElasticLowLevelRestClientFactory.getRestClient;
+
 @Slf4j
 public class PublishResults {
-    private static final ElasticsearchClient elasticsearchClient = getElasticHighLevelRestAPIClient();
+    private static final Config CONFIG = TestEnvFactory.getInstance().getConfig();
+    private static final ElasticServerChoices ELASTIC_SERVER = CONFIG.getEnum(ElasticServerChoices.class,"ELASTIC_SERVER");
 
+    private static final ElasticsearchClient elasticsearchClient = getElasticHighLevelRestAPIClient();
     public static void toElastic(TestRunMetaData testRunMetaData) throws IOException {
 
         IndexResponse response = elasticsearchClient.index(i -> i
-                .index("testproject")
+                .index("search-testproject")
                 .document(testRunMetaData)
         );
         
@@ -32,7 +38,7 @@ public class PublishResults {
 
     public static ElasticsearchClient getElasticHighLevelRestAPIClient(){
         // Create the low-level client
-        RestClient restClient = getRestClientForLocalHost();
+        RestClient restClient = ElasticLowLevelRestClientFactory.getRestClient(ELASTIC_SERVER);
 
         // Create the transport with a Jackson mapper
         ElasticsearchTransport transport = new RestClientTransport(
@@ -40,23 +46,5 @@ public class PublishResults {
 
         // And create the API client
         return new ElasticsearchClient(transport);
-    }
-
-    private static RestClient getRestClientForLocalHost() {
-        return RestClient.builder(
-                new HttpHost("localhost", 9200)).build();
-    }
-
-    private static RestClient getRestClientForElasticCloud(){
-        Header[] defaultHeaders =
-                new Header[]{
-                        new BasicHeader("Accept", "application/json"),
-                        new BasicHeader("Authorization", "ApiKey "),
-                };
-
-        return RestClient.builder(
-                new HttpHost("",443,"https"))
-                .setDefaultHeaders(defaultHeaders)
-                .build();
     }
 }
