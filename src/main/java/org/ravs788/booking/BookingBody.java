207691ae3009711fa98c2de302eace758263d604
Ravi Shankar <ravs788@gmail.com>
2025-03-27 06:41:40 +0530

    Introduced method chaining, including only non null values in request body and mutate objects using accessor pattern

--- src/main/java/org/ravs788/booking/BookingBody.java
+++ src/main/java/org/ravs788/booking/BookingBody.java
@@ -1,11 +1,21 @@
 package org.ravs788.booking;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
 import lombok.extern.slf4j.Slf4j;
 import org.ravs788.booking.entities.Bookingdates;
 
 @Slf4j
 @Data
+@NoArgsConstructor
+@Builder(setterPrefix = "set")
+@Accessors(chain = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@AllArgsConstructor
 public class BookingBody {
   private String firstname;
   private String lastname;
@@ -15,17 +25,19 @@ public class BookingBody {
   private String additionalneeds;
 
   public static BookingBody getInstance() {
-    Bookingdates bookingdates = new Bookingdates();
-    bookingdates.setCheckin("2018-01-01");
-    bookingdates.setCheckout("2019-01-01");
+    Bookingdates bookingdates =
+        Bookingdates.builder().setCheckin("2018-01-01").setCheckout("2019-01-01").build();
 
-    BookingBody bookingBody = new BookingBody();
-    bookingBody.setFirstname("Jim");
-    bookingBody.setLastname("Brown");
-    bookingBody.setTotalprice(111);
-    bookingBody.setDepositpaid(true);
-    bookingBody.setAdditionalneeds("Breakfast");
-    bookingBody.setBookingdates(bookingdates);
+    BookingBody bookingBody =
+        BookingBody.builder()
+            .setFirstname("Jim")
+            .setFirstname("Jim")
+            .setLastname("Brown")
+            .setTotalprice(111)
+            .setDepositpaid(true)
+            .setAdditionalneeds("Breakfast")
+            .setBookingdates(bookingdates)
+            .build();
 
     log.info("Booking body: {}", bookingBody);
     return bookingBody;
